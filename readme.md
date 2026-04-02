22d705c4afb81f2e0fa8b187a3aad6e4a0ab0934
B0LTedERR0R <jdb0lt@outlook.com>
2026-04-01 22:19:33 -0400

    made base stable config

---
+++ readme.md
@@ -0,0 +1,5 @@
+# Purpose
+make a Nixos flake that will be used for all my servers on the home lab
+
+# Inspiration
+
---
+++ system/default.nix
@@ -0,0 +1,14 @@
+{ ... }:
+
+{
+
+        imports = [
+                ./boot.nix
+                ./keyboard.nix
+                ./networking.nix
+                ./packages.nix
+                ./programs.nix
+                ./users.nix
+        ];
+
+}
